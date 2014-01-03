506482b0aa3c261e5640ab0a46b185364f9b3846
Graham Smith <graham.smith@lacoda.com>
2014-01-03 09:04:33 -0500

    Add parsing from rendered fields

--- test/formattersSpec.js
+++ test/formattersSpec.js
@@ -5,21 +5,43 @@ var _ = require('underscore'),
     expect = require('chai').expect;
 
 describe('Formatters', function() {
-    it('Should format cost', function() {
-        expect(formatters.cost(3.00)).to.equal('3.00');
-        expect(formatters.cost(3.1)).to.equal('3.10');
-        expect(formatters.cost(3.15)).to.equal('3.15');
-        expect(formatters.cost(3.156)).to.equal('3.16');
+    describe('Formatting', function() {
+        it('Should format cost', function() {
+            expect(formatters.cost.format(3.00)).to.equal('3.00');
+            expect(formatters.cost.format(3.1)).to.equal('3.10');
+            expect(formatters.cost.format(3.15)).to.equal('3.15');
+            expect(formatters.cost.format(3.156)).to.equal('3.16');
+        });
+
+        it('Should format dates', function() {
+            expect(formatters.date.format('2014-06-01')).to.equal('6/1/2014');
+            expect(formatters.date.format('2014-06-01T00:00:00')).to.equal('6/1/2014');
+        });
+
+        it('Should format percentages', function() {
+            expect(formatters.percent.format(0.3)).to.equal('30');
+            expect(formatters.percent.format(0.333)).to.equal('33');
+            expect(formatters.percent.format(0.335)).to.equal('34');
+        });
     });
 
-    it('Should format dates', function() {
-        expect(formatters.date('2014-06-01')).to.equal('6/1/2014');
-        expect(formatters.date('2014-06-01T00:00:00')).to.equal('6/1/2014');
-    });
+    describe('Parsing', function() {
+        it('Should parse cost', function() {
+            expect(formatters.cost.parse('3.00')).to.equal(3);
+            expect(formatters.cost.parse('3.10')).to.equal(3.1);
+            expect(formatters.cost.parse('3.156')).to.equal(3.156);
+        });
 
-    it('Should format percentages', function() {
-        expect(formatters.percent(0.3)).to.equal('30');
-        expect(formatters.percent(0.333)).to.equal('33');
-        expect(formatters.percent(0.335)).to.equal('34');
+        it('Should parse dates', function() {
+            expect(formatters.date.parse('2013-01-01')).to.equal('2013-01-01');
+            expect(formatters.date.parse('1/1/13')).to.equal('2013-01-01');
+            expect(formatters.date.parse('01/01/2013')).to.equal('2013-01-01');
+        });
+
+        it('Should parse percentages', function() {
+            expect(formatters.percent.parse('30')).to.equal(0.3);
+            expect(formatters.percent.parse('33')).to.equal(0.33);
+            expect(formatters.percent.parse('33.3')).to.equal(0.333);
+        });
     });
 });
---
+++ test/parseSpec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var expect = require('chai').expect,
+    jsdom = require('jsdom'),
+    _ = require('underscore'),
+    Model = require('Model'),
+    Formatters = require('Formatters');
+
+global.document = global.document || jsdom.jsdom();
+global.window = global.window || global.document.createWindow();
+
+var $ = require('jqueryify');
+
+describe('Parsing a rendered model', function() {
+    beforeEach(function() {
+        this.el = $('<form>' +
+            '<input type="text" name="name" value="a name" />' +
+            '<input type="text" name="price" value="5.10" />' +
+            '<input type="text" name="percent" value="32" />' +
+            '<input type="date" name="date" value="6/1/2014" />' +
+            '</form>');
+    });
+
+    it('Should parse from a form', function() {
+        var AdornModel = Model.extend({
+            schema: {
+                'name': null,
+                'price': Formatters.cost,
+                'percent': Formatters.percent,
+                'date': Formatters.date
+            }
+        });
+
+        var model = new AdornModel();
+        var update = model.parseForm(this.el);
+
+        expect(_.keys(update)).to.have.length(4);
+        expect(update.name).to.equal('a name');
+        expect(update.price).to.equal(5.1);
+        expect(update.percent).to.equal(0.32);
+        expect(update.date).to.equal('2014-06-01');
+    });
+});
