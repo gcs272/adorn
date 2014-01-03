eb182e9760de0673fd8ec0f844ed8032e8925f59
Graham Smith <graham.smith@lacoda.com>
2014-01-03 14:23:18 -0500

    Fix index exports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Model = require('lib/model.js'),
-    Formatters = require('lib/formatters.js');
+var Model = require('./lib/model.js'),
+    Formatters = require('./lib/formatters.js');
 
 module.exports = {
     model: Model,
